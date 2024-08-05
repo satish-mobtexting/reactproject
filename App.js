75f58ceb9b5de0c738720bb49abd56f9319dcb50
Satish <satish.k@mobtexting.com>
2024-08-05 07:35:28 +0000

    some chnages done

--- App.js
+++ App.js
@@ -54,7 +54,92 @@ import ReactDOM  from "react-dom/client";
     // root.render(container);
 
 
+    // creating a nested html using react.
+    const parent =  React.createElement(
+        "div",  // element name
+        {id:"parent"}, // properties
+        React.createElement(
+            "div",  // element name
+            {id:"child"}, // properties
+            React.createElement(
+                "h1",  // element name
+                {}, // properties
+                "its h1 tag" // children
+                )
+            )
+        );
 
+    // rendering the nested html using reactdom.
+    // const root = ReactDOM.createRoot(document.getElementById("root"));
+    // root.render(parent);
+
+        // creating a nested html using react.
+        const parentwithsiblings =  React.createElement(
+            "div",  // element name
+            {id:"parent"}, // properties
+            React.createElement(
+                "div",  // element name
+                {id:"child"}, // properties
+                [
+                    React.createElement(
+                    "h1",  // element name
+                    {}, // properties
+                    "its h1 tag" // children
+                    ),
+                    React.createElement(
+                    "h2",  // element name
+                    {}, // properties
+                    "its h2 tag" // children
+                    )
+                ]
+                )
+            );
+
+            // const root = ReactDOM.createRoot(document.getElementById("root"));
+            // root.render(parentwithsiblings);
+
+
+        const parentwithchildsiblings =  React.createElement(
+            "div",  // element name
+            {id:"parent"}, // properties
+            [   
+                React.createElement(
+                "div",  // element name
+                {id:"child1"}, // properties
+                [
+                    React.createElement(
+                    "h1",  // element name
+                    {}, // properties
+                    "its h1 tag" // children
+                    ),
+                    React.createElement(
+                    "h2",  // element name
+                    {}, // properties
+                    "its h2 tag" // children
+                    )
+                ]
+                ),
+                React.createElement(
+                    "div",  // element name
+                    {id:"child2"}, // properties
+                    [
+                        React.createElement(
+                        "h1",  // element name
+                        {}, // properties
+                        "its h1 tag" // children
+                        ),
+                        React.createElement(
+                        "h2",  // element name
+                        {}, // properties
+                        "its h2 tag" // children
+                        )
+                    ]
+                    )
+                ]
+            );
+
+    // const root = ReactDOM.createRoot(document.getElementById("root"));
+    // root.render(parentwithchildsiblings);
 
 
     // creating the elements using jsx.
@@ -110,7 +195,16 @@ import ReactDOM  from "react-dom/client";
     const HeaderComponent3 = function () {
         return (
             <div>
-                {header3} {/* we can write any javascript code */}
+                {header3} 
+                {/* we can write any javascript code 
+                
+                *
+                *
+                * is jsx mandatory ? no
+                * is typescript mandatory ? no 
+                * is es6 mandatory ? no
+                * 
+                */}
                 <h1>This is React functional component</h1>
                 <h1>This is h1 tag</h1>
                 <HeaderComponent2 />{/* this is know as component composition */}
@@ -119,8 +213,88 @@ import ReactDOM  from "react-dom/client";
     }
 
     //here {header3} inserting the react element into a react component. 
-    //here  <HeaderComponent2 /> inserting a react functional compont into another functional component.
+    // here  <HeaderComponent2 /> inserting a react functional component into another functional component.
     // <HeaderComponent2 /> or {HeaderComponent2()} both are same.
     // example of rendering the react componet.
-    // inserting a component inside a component is called as component coposition.
-    root.render(<HeaderComponent3 />)
+    // inserting a component inside another component is called as component coposition.
+    // root.render(<HeaderComponent3 />)
+
+
+    const Title = () => {
+        return (
+            <a href="/"><img alt="image" className="logo" src="https://com-mt-production-ap-south-1-assets.s3.ap-south-1.amazonaws.com/53660/0216ee4e-6e51-423b-bc88-ca95af378a97.jpeg?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIARNLZC3JYWGS5WCXC%2F20240309%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20240309T122010Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=28ba51e632ccb471aa7f697b6f81ae9ccd4594803cd0bd81bfde0d179f60ac42"></img></a>
+        );
+    }
+
+    const Header = () => {
+        return (
+            <div className="header">
+                <Title></Title>
+                <div className="navitems">
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Home</li>
+                        <li>Cart</li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+
+    const Body = () => {
+        return (
+            <h1>body</h1>
+        );
+    }
+
+    const Footer = () => {
+        return (
+            <h1>footer</h1>
+        );
+    }
+
+    // jsx expressions must have one parent element.
+    // const jsx = <h1>heading one</h1><h2>heading two</h2>  this is WRONG because jsx expression should have one parent element.
+    // const jsx = <div><h1>heading one</h1><h2>heading two</h2></div> this is CORRECT because all child tags are wrapped into one parent tag.
+
+    // Example: adding the empty <div></div> as a parent tag for the component.
+
+    // const styleObj = {
+    //  "background-color": "red",
+    // };
+
+    // const AppLayout = () => {
+    //     return (
+    //         <div style={styleObj}> // inline style in react.
+    //             <Header />
+    //             <Body />
+    //             <Footer />
+    //         </div>
+    //     );
+    // }
+
+    // React.Fragment is a component exported by the react. 
+    // so we can replace the empty <div></div> with <React.Fragment></React.Fragment>.  
+    // const AppLayout = () => {
+    //     return (
+    //         <React.Fragment>
+    //             <Header />
+    //             <Body />
+    //             <Footer />
+    //         </React.Fragment>
+    //     );
+    // }
+
+    // <React.Fragment></React.Fragment> is equal to <></>
+    const AppLayout = () => {
+        return (
+            <>
+                <Header />
+                <Body />
+                <Footer />
+            </>
+        );
+    }
+    
+    root.render(<AppLayout />);
